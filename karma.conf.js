613727d027d2e5764abad3fa3a37c32eff06def0
Andy Wermke <andy@dev.next-step-software.com>
2015-09-18 21:33:24 +0200

    Run tests on Firefox

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeInsecure'],
+    browsers: process.env.TRAVISCI ? ['Firefox'] : ['ChromeInsecure', 'Firefox'],
 
 
     customLaunchers: {
