10554892bd21de6769380d37d77368c47f80d7c0
Andy Wermke <andy@dev.next-step-software.com>
2016-06-17 08:16:21 +0200

    Remove extraneous blank lines from karma config

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,6 @@
 
 module.exports = function(config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
@@ -42,32 +41,24 @@ module.exports = function(config) {
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
 
-
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_DEBUG,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
-
     browserNoActivityTimeout: 10000,
-
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: process.env.TRAVISCI ? ['Firefox'] : ['ChromeInsecure', 'Firefox'],
-
 
     customLaunchers: {
       ChromeInsecure: {
@@ -75,7 +66,6 @@ module.exports = function(config) {
         flags: ['--disable-web-security', '--no-sandbox']
       }
     },
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
