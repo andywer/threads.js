95f050c7e18937b46f38ccc76b9af045ed52d176
Andy Wermke <andy@dev.next-step-software.com>
2017-11-24 17:00:56 +0100

    Merge dependent functional pool tests

--- test/spec/pool-functional.spec.js
+++ test/spec/pool-functional.spec.js
@@ -21,17 +21,10 @@ describe('Pool (functional test)', () => {
       jobs.push(job);
       promises.push(job.promise());
     }
-  });
 
-  it('responds as expected', done => {
-    Promise
-      .all(promises)
+    return Promise.all(promises)
       .then(responses => {
         expect(responses.sort()).to.eql([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
-        done();
       })
-      .catch(error => {
-        done(error);
-      });
   });
 });
