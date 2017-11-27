90b84f28d48ad289bed1566848bab7b4b4e24a48
Andy Wermke <a.wermke@gmail.com>
2017-11-27 08:32:26 +0100

    Add note about importScripts in node worker

--- src/worker.node/worker.js
+++ src/worker.node/worker.js
@@ -7,8 +7,9 @@ import { getConfig } from '../config';
 
 export default class Worker extends EventEmitter {
   constructor(initialRunnable, importScripts = [], options = {}) {
+    // `importScripts` cannot be consumed, it's just there to keep the API compatible to the browser worker
     super();
-
+    
     this.slave = child.fork(path.join(__dirname, 'slave.js'), [], options);
     this.slave.on('message', this.handleMessage.bind(this));
     this.slave.on('error', this.handleError.bind(this));
