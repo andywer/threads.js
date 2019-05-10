9b788a22eba6e197305a39f1a34f1ff9eae5c8da
Andy Wermke <andy.dev@next-step-software.com>
2019-05-10 18:54:57 +0200

    Fix test

--- test/transferables.test.ts
+++ test/transferables.test.ts
@@ -6,11 +6,30 @@ type SpyInit<Args extends any[], OriginalReturn, NewReturn> =
   (...args: Args) =>
   NewReturn
 
+const arrayBufferPlaceholder = Symbol("ArrayBufferPlaceholder")
+
 function spyOn<Args extends any[], OriginalReturn, NewReturn>(
   target: ((...args: Args) => OriginalReturn),
   spy: SpyInit<Args, OriginalReturn, NewReturn>
 ): (...args: Args) => NewReturn {
   return spy(target)
+}
+
+function replaceArrayBufferWithPlaceholder<In extends any>(obj: In, arrayBuffer: ArrayBuffer): In extends ArrayBuffer ? (In | typeof arrayBufferPlaceholder) : In {
+  if ((obj as any) === arrayBuffer) {
+    return arrayBufferPlaceholder as any
+  } else if (Array.isArray(obj)) {
+    return (obj as any[]).map(element => replaceArrayBufferWithPlaceholder(element, arrayBuffer)) as any
+  } else if (obj && typeof obj === "object") {
+    const result: typeof obj = Object.create(Object.getPrototypeOf(obj))
+
+    for (const key of Object.getOwnPropertyNames(obj)) {
+      result[key] = replaceArrayBufferWithPlaceholder(obj[key], arrayBuffer)
+    }
+    return result as any
+  } else {
+    return obj as any
+  }
 }
 
 test("can pass transferable objects on thread call", async t => {
@@ -20,7 +39,7 @@ test("can pass transferable objects on thread call", async t => {
   const postMessageCalls: Array<any[]> = []
 
   worker.postMessage = spyOn(worker.postMessage.bind(worker), postMessage => (...args) => {
-    postMessageCalls.push(args)
+    postMessageCalls.push(replaceArrayBufferWithPlaceholder(args, testData))
     return postMessage(...args)
   })
 
@@ -30,12 +49,12 @@ test("can pass transferable objects on thread call", async t => {
   t.is(postMessageCalls.length, 1)
   t.is(postMessageCalls[0].length, 2)
   t.deepEqual(postMessageCalls[0][0], {
-    args: [testData, 15],
+    args: [arrayBufferPlaceholder, 15],
     method: undefined,
     type: "run",
     uid: postMessageCalls[0][0].uid
   })
-  t.deepEqual(postMessageCalls[0][1], [testData])
+  t.deepEqual(postMessageCalls[0][1], [arrayBufferPlaceholder])
 
   await Thread.terminate(xorBuffer)
 })
