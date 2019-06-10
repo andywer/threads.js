88b2e24c223272f704f1e8f610f4b27e045c0e57
Andy Wermke <andy.dev@next-step-software.com>
2019-06-10 19:11:15 +0200

    Print webpack build errors during testing

--- test/webpack/webpack.test.ts
+++ test/webpack/webpack.test.ts
@@ -4,6 +4,15 @@ import Webpack from "webpack"
 const browserConfig = require("./webpack.web.config")
 const serverConfig = require("./webpack.node.config")
 
+const stringifyWebpackError = (error: any) =>
+  !error
+  ? ""
+  : typeof error.stack === "string"
+  ? error.stack
+  : typeof error.message === "string"
+  ? error.message
+  : error
+
 async function runWebpack(config: any) {
   return new Promise<Webpack.Stats>((resolve, reject) => {
     Webpack(config).run((error, stats) => {
@@ -14,12 +23,12 @@ async function runWebpack(config: any) {
 
 test("can create a browser bundle with webpack", async t => {
   const stats = await runWebpack(browserConfig)
-  t.deepEqual(stats.compilation.errors, [])
+  t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
 })
 
 test("can create a working server bundle with webpack", async t => {
   const stats = await runWebpack(serverConfig)
-  t.deepEqual(stats.compilation.errors, [])
+  t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
 
   const bundle = require("./dist.node/main")
   await bundle.test()
