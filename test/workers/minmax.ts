c7c48e44d410530dc0be44e96e376fe8c7fe81e0
Andy Wermke <andy.dev@next-step-software.com>
2019-06-18 20:59:54 +0200

    Document and test streaming worker using observable subject

---
+++ test/workers/minmax.ts
@@ -0,0 +1,25 @@
+import "ts-node/register"
+import { Observable, Subject } from "../../src/observable"
+import { expose } from "../../src/worker"
+
+let max = -Infinity
+let min = Infinity
+
+let subject = new Subject()
+
+const minmax = {
+  finish() {
+    subject.complete()
+    subject = new Subject()
+  },
+  push(value) {
+    max = Math.max(max, value)
+    min = Math.min(min, value)
+    subject.next({ max, min })
+  },
+  values() {
+    return Observable.from(subject)
+  }
+}
+
+expose(minmax)
