66f715232c7ef1ddd57b0dc39539f808f0cab8a5
Andy Wermke <andy.dev@next-step-software.com>
2019-09-05 21:19:17 +0200

    Use threads-plugin v1.1.0 and add regression test (#158)

--- test/webpack/app.ts
+++ test/webpack/app.ts
@@ -1,9 +1,10 @@
 import { spawn, Pool, Worker } from "../../src/index"
 
+type AdditionWorker = (a: number, b: number) => number
 type HelloWorker = (text: string) => string
 
 async function test() {
-  const pool = await Pool(() => spawn<HelloWorker>(new Worker("./worker")))
+  const pool = Pool(() => spawn<HelloWorker>(new Worker("./pool-worker")))
   const results = await Promise.all([
     pool.queue(hello => hello("World")),
     pool.queue(hello => hello("World")),
@@ -14,9 +15,24 @@ async function test() {
 
   for (const result of results) {
     if (result !== "Hello, World") {
-      throw Error("Unexpected result returned by worker: " + result)
+      throw Error("Unexpected result returned by pool worker: " + result)
     }
   }
 }
 
-export default test
+async function test2() {
+  // We also want to test if referencing multiple different workers in a module
+  // built using webpack works
+
+  const add = await spawn<AdditionWorker>(new Worker("./addition-worker"))
+  const result = await add(2, 3)
+
+  if (result !== 5) {
+    throw Error("Unexpected result returned by addition worker: " + result)
+  }
+}
+
+export default () => Promise.all([
+  test(),
+  test2()
+])
