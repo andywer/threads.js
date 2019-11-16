218f1c67332b48e242fff2789f50847631ae7352
Andy Wermke <github@andywer.com>
2019-11-16 19:12:55 +0100

    Work around security error when master is on a file:// URL (#177)
    As seen in Solar wallet on Safari.

--- src/master/implementation.browser.ts
+++ src/master/implementation.browser.ts
@@ -1,8 +1,11 @@
+// tslint:disable max-classes-per-file
+
 import { ThreadsWorkerOptions, WorkerImplementation } from "../types/master"
+import { getBundleURL } from "./get-bundle-url.browser"
 
 const defaultPoolSize = navigator.hardwareConcurrency || 4
 
-const isAbsoluteURL = (value: string) => /^(https?:)?\/\//i.test(value)
+const isAbsoluteURL = (value: string) => /^(file|https?:)?\/\//i.test(value)
 
 function createSourceBlobURL(code: string): string {
   const blob = new Blob(
@@ -14,10 +17,23 @@ function createSourceBlobURL(code: string): string {
 
 
 function selectWorkerImplementation(): typeof WorkerImplementation {
+  if (typeof Worker === "undefined") {
+    // Might happen on Safari, for instance
+    // The idea is to only fail if the constructor is actually used
+    return class NoWebWorker {
+      constructor() {
+        throw Error("No web worker implementation available. You might have tried to spawn a worker within a worker in a browser that doesn't support workers in workers.")
+      }
+    } as any
+  }
+
   return class WebWorker extends Worker {
     constructor(url: string | URL, options?: ThreadsWorkerOptions) {
       if (typeof url === "string" && options && options._baseURL) {
         url = new URL(url, options._baseURL)
+      } else if (typeof url === "string" && !isAbsoluteURL(url) && getBundleURL().match(/^file:\/\//i)) {
+        url = new URL(url, getBundleURL().replace(/\/[^\/]+$/, "/"))
+        url = createSourceBlobURL(`importScripts(${JSON.stringify(url)});`)
       }
       if (typeof url === "string" && isAbsoluteURL(url)) {
         // Create source code blob loading JS file via `importScripts()`
