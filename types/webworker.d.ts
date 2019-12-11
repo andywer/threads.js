7d87e003dc103f5f1a50dc1590f238bf20c9364f
Andy Wermke <github@andywer.com>
2019-12-11 04:59:32 +0100

    Make sure types work with tsconfig `lib` set to `dom`, too

--- types/webworker.d.ts
+++ types/webworker.d.ts
@@ -1,3 +1,6 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="webworker" />
+
 interface WorkerGlobalScope {
   addEventListener(eventName: string, listener: (event: Event) => void): void
   postMessage(message: any, transferables?: any[]): void
