a8e71ebf5ad790571578264e98e879047e427993
Andy Wermke <github@andywer.com>
2020-06-12 10:07:20 +0200

    Add BlobWorker tests

--- test/webpack/webpack.test.ts
+++ test/webpack/webpack.test.ts
@@ -1,4 +1,5 @@
 import test from "ava"
+import * as path from "path"
 import Webpack from "webpack"
 
 const browserConfig = require("./webpack.web.config")
@@ -30,6 +31,58 @@ test("can create a working server bundle with webpack", async t => {
   const stats = await runWebpack(serverConfig)
   t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
 
-  const bundle = require("./dist.node/main")
+  const bundle = require("./dist/app.node/main")
   await bundle.test()
+})
+
+test("can inline a worker into an app bundle", async t => {
+  // Bundle browser worker
+  let stats = await runWebpack({
+    ...browserConfig,
+    entry: require.resolve("./addition-worker"),
+    output: {
+      filename: "worker.js",
+      path: path.resolve(__dirname, "dist/addition-worker.web")
+    },
+    target: "webworker"
+  })
+  t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
+
+  // Bundle server worker
+  stats = await runWebpack({
+    ...serverConfig,
+    entry: require.resolve("./addition-worker"),
+    output: {
+      filename: "worker.js",
+      path: path.resolve(__dirname, "dist/addition-worker.node")
+    }
+  })
+  t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
+
+  // Bundle browser app
+  stats = await runWebpack({
+    ...browserConfig,
+    entry: require.resolve("./app-with-inlined-worker"),
+    output: {
+      ...serverConfig.output,
+      path: path.resolve(__dirname, "dist/app-inlined.web")
+    }
+  })
+  t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
+
+  // Bundle server app
+  stats = await runWebpack({
+    ...serverConfig,
+    entry: require.resolve("./app-with-inlined-worker"),
+    output: {
+      ...serverConfig.output,
+      path: path.resolve(__dirname, "dist/app-inlined.node")
+    }
+  })
+  t.deepEqual(stats.compilation.errors, [], stringifyWebpackError(stats.compilation.errors[0]))
+
+  const bundle = require("./dist/app-inlined.node/main")
+  const result = await bundle.test()
+
+  t.is(result, "test succeeded")
 })
