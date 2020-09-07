f683da4e2e8a3d3140bae6d38875e9e8ad4f5f65
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-07 09:10:52 -0500

    use `@rollup` plugins

--- test/rollup/rollup.config.ts
+++ test/rollup/rollup.config.ts
@@ -1,5 +1,5 @@
-import commonjs from "rollup-plugin-commonjs"
-import nodeResolve from "rollup-plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
 
 export default {
   plugins: [
