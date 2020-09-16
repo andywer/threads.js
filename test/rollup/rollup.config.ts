de9fe5999b8b8e05ba4f4d925931d071d42c4791
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-16 02:59:10 -0500

    add ts-ignore to rollup.config.ts

--- test/rollup/rollup.config.ts
+++ test/rollup/rollup.config.ts
@@ -1,14 +1,21 @@
 /* tslint:disable */
-let commonjs, nodeResolve;
+//@ts-ignore
+let commonjs, nodeResolve; //@ts-ignore
+//@ts-ignore
 if (parseFloat(process.version.match(/^v(\d+\.\d+)/)[1]) < 10) {
+  //@ts-ignore
   commonjs = require("rollup-plugin-commonjs");
+  //@ts-ignore
   nodeResolve = require("rollup-plugin-node-resolve");
 } else {
+  //@ts-ignore
   commonjs = require("@rollup/plugin-commonjs");
+  //@ts-ignore
   nodeResolve = require("@rollup/plugin-node-resolve").nodeResolve;
 }
 
-module.exports = {
+//@ts-ignore
+export default {
   plugins: [
     nodeResolve({
       browser: true,
