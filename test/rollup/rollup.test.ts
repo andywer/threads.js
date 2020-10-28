c3cc39ac65a0eca071daa5a986a441bbd465a395
aminya <aminyahyaabadi74@gmail.com>
2020-10-28 06:21:20 -0500

    await Rollup promises independently
    The ci timeouts!

--- test/rollup/rollup.test.ts
+++ test/rollup/rollup.test.ts
@@ -6,27 +6,28 @@ import config from "./rollup.config"
 
 test("can be bundled using rollup", async t => {
   t.timeout(2000000); // milliseconds
-  const [appBundle, workerBundle] = await Promise.all([
-    rollup({
-      input: path.resolve(__dirname, "app.js"),
-      ...config
-    }),
-    rollup({
-      input: path.resolve(__dirname, "worker.js"),
-      ...config
-    })
-  ])
 
-  await Promise.all([
-    appBundle.write({
-      dir: path.resolve(__dirname, "dist"),
-      format: "iife"
-    }),
-    workerBundle.write({
-      dir: path.resolve(__dirname, "dist"),
-      format: "iife"
-    })
-  ])
+  const appBundleP = rollup({
+     input: path.resolve(__dirname, "app.js"),
+    ...config
+  })
+
+  const workerBundleP = rollup({
+    input: path.resolve(__dirname, "worker.js"),
+    ...config
+  })
+
+  const appBundleWriteP = (await appBundleP).write({
+    dir: path.resolve(__dirname, "dist"),
+    format: "iife"
+  })
+
+  const workerBundleWriteP = (await workerBundleP).write({
+    dir: path.resolve(__dirname, "dist"),
+    format: "iife"
+  })
+
+  await Promise.all([appBundleWriteP, workerBundleWriteP])
 
   if (process.platform === "win32") {
     // Quick-fix for weird Windows issue in CI
