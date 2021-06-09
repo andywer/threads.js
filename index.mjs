fe9a2b4614470fe388d1a8162be80ec3ac3a2a15
Andy Wermke <github@andywer.com>
2021-06-09 20:40:39 +0200

    Export BlobWorker from mjs entrypoint (#379)
    Fixes #378.

--- index.mjs
+++ index.mjs
@@ -2,6 +2,7 @@ import Threads from "./dist/index.js"
 
 export const registerSerializer = Threads.registerSerializer
 export const spawn = Threads.spawn
+export const BlobWorker = Threads.BlobWorker
 export const DefaultSerializer = Threads.DefaultSerializer
 export const Pool = Threads.Pool
 export const Thread = Threads.Thread
