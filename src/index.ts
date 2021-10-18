9828320d18c31691fc3fcb02bdad76fdbb8562dd
Juho Vepsäläinen <bebraw@gmail.com>
2021-10-18 20:28:26 +0300

    chore: Drop ;'s

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export { registerSerializer } from "./common";
-export * from "./master/index";
-export { expose } from "./worker/index";
-export { expose as exposeShared } from "./shared-worker/index";
+export { registerSerializer } from "./common"
+export * from "./master/index"
+export { expose } from "./worker/index"
+export { expose as exposeShared } from "./shared-worker/index"
 export {
   DefaultSerializer,
   JsonSerializable,
   Serializer,
   SerializerImplementation,
-} from "./serializers";
-export { Transfer, TransferDescriptor } from "./transferable";
+} from "./serializers"
+export { Transfer, TransferDescriptor } from "./transferable"
