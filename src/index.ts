7ef6eed7581a88643b3acc962bc8ae2402be3165
Sebastian Trebunak <33039909+sebqq@users.noreply.github.com>
2022-02-16 11:54:35 +0100

    Re-export pool related types in order to make them accessible for users (#373)
    * Re-export pool related types in order to make them accesible
    
    * Add new-line at the end of index.js file
    
    Co-authored-by: Sebastian Trebunak <strebunak@abboptical.com>

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from "./master/index"
 export { expose } from "./worker/index"
 export { DefaultSerializer, JsonSerializable, Serializer, SerializerImplementation } from "./serializers"
 export { Transfer, TransferDescriptor } from "./transferable"
+export { ExposedToThreadType as ExposedAs } from "./master/spawn";
+export { QueuedTask } from "./master/pool";
