31bfd44cae7e6e036a66a20096ee5bc3a9228730
Antonio Musolino <antoniomusolino007@gmail.com>
2022-03-18 06:20:42 +0100

    fix: missing isWorkerRuntimer export in esm (#419)

--- worker.mjs
+++ worker.mjs
@@ -3,3 +3,4 @@ import WorkerContext from "./dist/worker/index.js"
 export const expose = WorkerContext.expose
 export const registerSerializer = WorkerContext.registerSerializer
 export const Transfer = WorkerContext.Transfer
+export const isWorkerRuntime = WorkerContext.isWorkerRuntime
